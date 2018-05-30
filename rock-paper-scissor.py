c44d158035c533cc80ab60fab05dab7349e5e9ca
alexob1212 <atobrie@umich.edu>
2018-05-30 16:22:48 -0400

    Update rock-paper-scissor.py

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -7,20 +7,26 @@ player2 = input("(enter Player 2's choice): ")
 
 print("Shoot!")
 
-if player1 == "Rock" and player2 == "Paper":
-	print("Player 2 wins!")
-elif player1 == "Rock" and player2 == "Scissors":
-	print("Player 1 wins!")
+if player1 == "Rock":
+	if player2 == "Paper":
+		print("Player 2 wins!")
+	elif player2 == "Scissors":
+		print("Player 1 wins!")
 
-elif player1 == "Paper" and player2 == "Rock":
-	print("Player 1 wins!")
-elif player1 == "Paper" and player2 == "Scissors":
-	print("Player 2 wins!")
+elif player1 == "Paper":
+	if player2 == "Rock":
+		print("Player 1 wins!")
+	elif player2 == "Scissors":
+		print("Player 2 wins!")
 
-elif player1 == "Scissors" and player2 == "Rock":
-	print("Player 2 wins!")
-elif player1 == "Scissors" and player2 == "Paper":
-	print("Player 1 wins!")
+elif player1 == "Scissors":
+	if  player2 == "Rock":
+		print("Player 2 wins!")
+	elif player2 == "Paper":
+		print("Player 1 wins!")
+
+elif player1 == player2:
+	print("Tie!")
 
 else:
-	print("Draw!")
+	print("Error.")
